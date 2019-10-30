47d6743be22485781fa50d325c660c67c92345f9
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2019-10-30 06:54:19 +0200

    Adding a Responsive Form to our Context App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import Navbar from './Navbar'
+import React from "react";
+import Navbar from "./Navbar";
+import Form from "./Form";
 
 function App() {
-  return (
-    <Navbar  />
-  );
+    return (
+        <>
+            <Navbar></Navbar>
+            <Form></Form>
+        </>
+    );
 }
 
 export default App;
