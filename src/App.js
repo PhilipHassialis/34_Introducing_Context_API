27689ab444895c5091451c1ce442547722c6f0c5
Chasialis Philip <ChasialisP@unisystems.gr>
2019-10-30 17:42:54 +0200

    Intro to Context and Providers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Form from "./Form";
-
+import PageContent from './PageContent'
+import {ThemeProvider} from './contexts/ThemeContext'
+ 
 function App() {
     return (
-        <>
-            <Navbar></Navbar>
-            <Form></Form>
-        </>
+        <ThemeProvider>
+            <PageContent>
+                <Navbar></Navbar>
+                <Form></Form>
+            </PageContent>
+        </ThemeProvider>
+
     );
 }
 
