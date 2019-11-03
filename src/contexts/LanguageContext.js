4185d0a74aa962376f42288de762a49d8193e07b
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2019-11-03 10:11:01 +0200

    Rewriting a Context Provider w/hooks

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,22 +1,12 @@
-import React, { Component, createContext } from "react";
+import React, { useState, createContext } from "react";
 
 export const LanguageContext = createContext();
 
-export class LanguageProvider extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { language: "greek" };
-    }
-
-    changeLanguage = e => {
-        this.setState({ language: e.target.value });
+export const LanguageProvider = props => {
+    const [language, setLanguage] = useState("greek");
+    const changeLanguage = e => {
+        setLanguage(e.target.value);
     };
 
-    render() {
-        return (
-            <LanguageContext.Provider value={{ ...this.state, changeLanguage: this.changeLanguage }}>
-                {this.props.children}
-            </LanguageContext.Provider>
-        );
-    }
-}
+    return <LanguageContext.Provider value={{ language, changeLanguage }}>{props.children}</LanguageContext.Provider>;
+};
