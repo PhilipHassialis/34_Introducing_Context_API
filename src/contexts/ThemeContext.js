f536c03414af59bf258b517ce9e03eca6245ec1f
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2019-11-03 10:20:41 +0200

    Context providers w/ custom hooks

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,27 +1,10 @@
-import React, { Component, createContext } from 'react';
+import React, { createContext } from "react";
+import useToggleState from "../hooks/useToggleState";
 
 export const ThemeContext = createContext();
 
-export class ThemeProvider  extends Component {
+export const ThemeProvider = props => {
+    const [isDarkMode, toggleTheme] = useToggleState(true);
 
-    constructor(props) {
-        super(props)     
-        this.state = { isDarkMode: true }                                                                   
-    }
-    
-    toggleTheme = () => {
-        this.setState({isDarkMode:!this.state.isDarkMode})
-    }
-
-
-    render() {
-        return (
-            <ThemeContext.Provider value={{...this.state, toggleTheme:this.toggleTheme}}>
-                {this.props.children}
-            </ThemeContext.Provider>
-        )
-    }
-}
-
-
-
+    return <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>{props.children}</ThemeContext.Provider>;
+};
